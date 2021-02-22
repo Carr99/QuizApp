1b1c96ed56cb0bfa8c137281710776cc8654b241
Carr9799 <andre@carrblad.se>
2021-02-22 15:09:08 +0100

    Login and signup

--- app/src/main/java/com/example/quizgame/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/quizgame/fragments/LoginFragment.kt
@@ -2,13 +2,42 @@ package com.example.quizgame.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.quizgame.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val auth = Firebase.auth
 
+        val email = view.findViewById<EditText>(R.id.email_text)
+        val pass = view.findViewById<EditText>(R.id.password_text)
+        val loginButton = view.findViewById<Button>(R.id.login_button)
+        val signUpButton = view.findViewById<Button>(R.id.signUp_button)
+
+        loginButton.setOnClickListener {
+            activity?.let { it1 ->
+                auth.signInWithEmailAndPassword(email.text.toString(), pass.text.toString())
+                    .addOnCompleteListener(it1) { task ->
+                        if (task.isSuccessful) {
+                            val action = LoginFragmentDirections.actionLoginFragmentToMenuFragment()
+                            findNavController().navigate(action)
+                        } else {
+                            Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+        signUpButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToSignUpFragment()
+            findNavController().navigate(action)
+        }
     }
 }
--- app/src/main/java/com/example/quizgame/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/quizgame/fragments/SignUpFragment.kt
@@ -1,14 +1,72 @@
 package com.example.quizgame.fragments
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.quizgame.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUpFragment : Fragment(R.layout.fragment_signup) {
+    private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
 
+        val email = view.findViewById<EditText>(R.id.email)
+        val username = view.findViewById<EditText>(R.id.username)
+        val pass1 = view.findViewById<EditText>(R.id.password1)
+        val pass2 = view.findViewById<EditText>(R.id.password2)
+        val confirmBtn = view.findViewById<Button>(R.id.confirm_button)
+        val cancelBtn = view.findViewById<Button>(R.id.cancel_button)
+
+        cancelBtn.setOnClickListener {
+            val action = SignUpFragmentDirections.actionSignUpFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+        confirmBtn.setOnClickListener {
+            if (!TextUtils.isEmpty(email.text.toString()) || !TextUtils.isEmpty(username.text.toString()) || !TextUtils.isEmpty(pass1.text.toString()) || !TextUtils.isEmpty(pass2.text.toString())) {
+                if (pass1.text.toString() == pass2.text.toString()) {
+                    activity?.let { it1 ->
+                        auth.createUserWithEmailAndPassword(email.text.toString(), pass1.text.toString()).addOnCompleteListener(it1) { task ->
+                            if (task.isSuccessful) {
+                                Log.d(TAG, "createUserWithEmail:success")
+                                val user = auth.currentUser
+                                if (user != null) {
+                                    val name = hashMapOf("username" to username.text.toString())
+                                    db.collection("users").document(user.uid).set(name)
+                                }
+                            } else {
+                                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                                Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                } else {
+                    pass2.error = "Not matching"
+                }
+            } else {
+                if (TextUtils.isEmpty(email.text.toString())) {
+                    email.error = "Required"
+                } else if (TextUtils.isEmpty(username.text.toString())) {
+                    username.error = "Required"
+                } else if (TextUtils.isEmpty(pass1.text.toString())) {
+                    pass1.error = "Minimum 6 characters"
+                } else if (TextUtils.isEmpty(pass2.text.toString())) {
+                    pass2.error = "Required"
+                }
+            }
+        }
     }
 }
