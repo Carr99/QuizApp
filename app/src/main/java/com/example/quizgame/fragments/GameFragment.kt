271132e4445a2a338158dd4d411e12fb874e5186
Ostbergc <chrilleostberg@hotmail.com>
2021-02-24 15:03:02 +0100

    GameFragment design, Home & Icon

--- app/src/main/java/com/example/quizgame/fragments/GameFragment.kt
+++ app/src/main/java/com/example/quizgame/fragments/GameFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.quizgame.R
 
+
 class GameFragment : Fragment(R.layout.fragment_game) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
