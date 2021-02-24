b35b15084eb2475bcea2ef8d2b62b0d646cf352b
simpiis <simon.ejderbrand@hotmail.com>
2021-02-24 15:04:05 +0100

    start of db gamefragment

--- app/src/main/java/com/example/quizgame/fragments/GameFragment.kt
+++ app/src/main/java/com/example/quizgame/fragments/GameFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.quizgame.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class GameFragment : Fragment(R.layout.fragment_game) {
-
+    private val db = Firebase.firestore
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // load all info
+
+    }
+
+
+    // save score etc to db
+    fun saveToDB(){
 
     }
 }
