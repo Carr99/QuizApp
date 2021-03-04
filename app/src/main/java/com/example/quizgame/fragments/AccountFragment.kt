76b07dae381a5f284a626b032753f74d5fd9274c
Ostbergc <chrilleostberg@hotmail.com>
2021-03-04 12:08:10 +0100

    Accountfragment & Resultfragment fixed

--- app/src/main/java/com/example/quizgame/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/quizgame/fragments/AccountFragment.kt
@@ -1,7 +1,11 @@
 package com.example.quizgame.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.quizgame.R
 
@@ -9,6 +13,17 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val changePasswordButton = view.findViewById<Button>(R.id.changePasswordButtonId)
+        val usernameTextView = view.findViewById<TextView>(R.id.usernameTextViewId)
+        val emailTextView = view.findViewById<TextView>(R.id.emailTextViewId)
+
+        val shared = activity?.getSharedPreferences("user_data", Context.MODE_PRIVATE)
+        val username = shared?.getString("username", "")
+
+        if (username != null){
+            usernameTextView.text = username.toString()
+        }
 
     }
 }
--- app/src/main/java/com/example/quizgame/fragments/ResultFragment.kt
+++ app/src/main/java/com/example/quizgame/fragments/ResultFragment.kt
@@ -2,6 +2,9 @@ package com.example.quizgame.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.quizgame.R
 
@@ -9,6 +12,15 @@ class ResultFragment : Fragment(R.layout.fragment_result) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val yourName = view.findViewById<TextView>(R.id.yourNameTextView)
+        val opponentName = view.findViewById<TextView>(R.id.opponentNameTextView)
+        val yourScore = view.findViewById<TextView>(R.id.yourScoreTextView)
+        val opponentScore = view.findViewById<TextView>(R.id.opponentScoreTextView)
+        val rateGameLayout = view.findViewById<LinearLayout>(R.id.rateGameId)
+        val ratingBar = view.findViewById<RatingBar>(R.id.ratingBarId)
+        val facebookButtonLayout = view.findViewById<LinearLayout>(R.id.facebookButton)
+
 
     }
 }
