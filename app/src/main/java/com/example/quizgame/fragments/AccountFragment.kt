30b95013c8e2ffa415991c9b0f6256fd112cb887
Ostbergc <chrilleostberg@hotmail.com>
2021-03-04 13:28:28 +0100

    logo on some fragments

--- app/src/main/java/com/example/quizgame/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/quizgame/fragments/AccountFragment.kt
@@ -7,7 +7,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.quizgame.R
+import com.google.firebase.auth.FirebaseAuth
 
 class AccountFragment : Fragment(R.layout.fragment_account) {
 
@@ -17,6 +19,7 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
         val changePasswordButton = view.findViewById<Button>(R.id.changePasswordButtonId)
         val usernameTextView = view.findViewById<TextView>(R.id.usernameTextViewId)
         val emailTextView = view.findViewById<TextView>(R.id.emailTextViewId)
+        val logOutButton = view.findViewById<Button>(R.id.logOutButtonId)
 
         val shared = activity?.getSharedPreferences("user_data", Context.MODE_PRIVATE)
         val username = shared?.getString("username", "")
@@ -25,5 +28,10 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
             usernameTextView.text = username.toString()
         }
 
+        logOutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut();
+            val action = AccountFragmentDirections.actionAccountFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
     }
 }
